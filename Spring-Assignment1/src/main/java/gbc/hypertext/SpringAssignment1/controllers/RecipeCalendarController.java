22d8458833d7bc474a2fab3ab82e07cd748bc069
Paolo <74078018+paoloreus@users.noreply.github.com>
2021-12-03 09:59:11 -0800

    Add files via upload

--- Spring-Assignment1/src/main/java/gbc/hypertext/SpringAssignment1/controllers/RecipeCalendarController.java
+++ Spring-Assignment1/src/main/java/gbc/hypertext/SpringAssignment1/controllers/RecipeCalendarController.java
@@ -1,19 +1,17 @@
-/*********************************************************************************
- * Project: HypertextAssassinsGBC
- * Assignment: Assignment #1
- * Author(s): Duncan Wardlaw, Oliver Kmiec, Paolo Tous
- * Student Number: 101247506, 101247765, 101325245
- * Date: November 7th, 2021
- * Description: out of scope could not finish
- *********************************************************************************/
-
 package gbc.hypertext.SpringAssignment1.controllers;
 
+import gbc.hypertext.SpringAssignment1.model.RecipeCalendar;
 import gbc.hypertext.SpringAssignment1.repository.RecipeCalendarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.Calendar;
 
 @Controller
 public class RecipeCalendarController {
@@ -24,6 +22,47 @@ public class RecipeCalendarController {
     @GetMapping({"/viewCalendar"})
     public String viewCalendar(Model model) {
         model.addAttribute("calendar", recipeCalendarRepository.findAll());
+        return "/user/viewCalendar";
+    }
+
+    @GetMapping({"/createMeal"})
+    public String createMeal(Model model){
+        model.addAttribute("meal", new RecipeCalendar());
+        return "/user/createMeal";
+    }
+
+    @PostMapping({"/createMeal"})
+    public String registerMeal(RecipeCalendar recipeCalendar, Model model){
+        model.addAttribute("calendar", recipeCalendarRepository.findAll());
+        return "/user/viewCalendar";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String updateEvent(@PathVariable("id") long id, Model model){
+        RecipeCalendar recipeCalendar = recipeCalendarRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Event ID: " + id));
+        model.addAttribute("meal", recipeCalendar);
+        return "update-event";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateEvent(@PathVariable("id") long id, @Valid RecipeCalendar recipeCalendar,
+                              BindingResult result, Model model){
+
+        if(result.hasErrors()){
+            recipeCalendar.setId(id);
+            return "update-event";
+        }
+
+        recipeCalendarRepository.save(recipeCalendar);
+        return "/user/viewCalendar";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEvent(@PathVariable("id") long id, Model model){
+        RecipeCalendar recipeCalendar = recipeCalendarRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Event ID: " + id));
+        recipeCalendarRepository.delete(recipeCalendar);
         return "/user/viewCalendar";
     }
 
