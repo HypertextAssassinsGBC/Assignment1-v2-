b50d31d8c40347cd9c7c34eb5dad98ca07546bd2
Paolo <74078018+paoloreus@users.noreply.github.com>
2021-12-03 09:58:28 -0800

    Add files via upload

--- Spring-Assignment1/src/main/java/gbc/hypertext/SpringAssignment1/repository/RecipeCalendarRepository.java
+++ Spring-Assignment1/src/main/java/gbc/hypertext/SpringAssignment1/repository/RecipeCalendarRepository.java
@@ -1,19 +1,7 @@
-/*********************************************************************************
- * Project: HypertextAssassinsGBC
- * Assignment: Assignment #1
- * Author(s): Duncan Wardlaw, Oliver Kmiec, Paolo Tous
- * Student Number: 101247506, 101247765, 101325245
- * Date: November 7th, 2021
- * Description: persists recipe entity's to the db
- *********************************************************************************/
-
 package gbc.hypertext.SpringAssignment1.repository;
 
 import gbc.hypertext.SpringAssignment1.model.RecipeCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RecipeCalendarRepository extends JpaRepository<RecipeCalendar, Long> {
-
 }
